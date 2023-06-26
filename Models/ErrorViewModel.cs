5741d217518a0076a15f388335b4a7e427d8bdb4
marianaleitt <marianaleitt@gmail.com>
2023-06-26 12:04:44 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LigaNos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
